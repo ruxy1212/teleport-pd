238251b74ba1058218be44eeda606afdd343135f
ruxy1212 <russelloje@gmail.com>
2024-07-09 11:24:16 +0100

    From second laptop

--- src/layouts/WebLayout.jsx
+++ src/layouts/WebLayout.jsx
@@ -5,8 +5,11 @@ import Email from '../components/icons/Email';
 import Phone from '../components/icons/Phone';
 import Close from '../assets/img/icons/Close.svg';
 import Menu from '../assets/img/icons/Menu.svg';
+import { useEffect, useState } from 'react';
 
 export default function WebLayout({children}){
+    const [open, setOpen] = useState(false);
+
     return (
         <>
             <nav className="flex fixed top-0 left-0 w-full z-30 justify-between items-center self-stretch py-2 px-6 bg-pd-white max-w-[1200px] mx-auto">
@@ -25,7 +28,7 @@ export default function WebLayout({children}){
                     <button className="py-4 px-9 flex items-center gap-2 bg-pd-black text-pd-white rounded-[3.25rem] font-medium pd-button font-montserrat">Go To Cart<ArrowRight className="h-5 w-5"/></button>
                 </div>
                 <div className="flex items-center gap-2 py-2 md:hidden">
-                    <button className="flex w-12 h-12 justify-center items-center">
+                    <button className="flex w-12 h-12 justify-center items-center" onClick={()=>setOpen(true)} >
                         <img src={Menu} alt="" />
                     </button>
                     {/* <button className="py-4 px-9 flex items-center gap-2 bg-pd-black text-pd-white rounded-[3.25rem] font-medium pd-button font-montserrat">Go To Cart<ArrowRight className="h-5 w-5"/></button>
@@ -33,14 +36,14 @@ export default function WebLayout({children}){
                      */}
                 </div>
             </nav>
-            <div className="hidden md:hidden fixed top-0 left-0 w-screen h-screen z-40 bg-pd-black/50 justify-end">
-                <nav className="relative right-0 w-[80%] w-max-[300px] z-40 bg-pd-white rounded-s-3xl py-2 px-6">
+            <div className={`${open?'flex':'hidden'} md:hidden fixed top-0 left-0 w-screen h-screen z-40 bg-pd-black/50 justify-end`}>
+                <nav className="relative right-0 w-[80%] w-max-[300px] z-40 bg-pd-white rounded-s-3xl py-2 px-6" onBlur={()=>setOpen(false)}>
                     <div className="flex justify-end py-2">
-                        <button className="flex w-12 h-12 justify-center items-center">
+                        <button className="flex w-12 h-12 justify-center items-center" onClick={()=>setOpen(false)}>
                             <img src={Close} alt="" />
                         </button>
                     </div>
-                    <div className="py-16 flex-col justify-between" style={{ height: 'calc(100% - 64px)' }}>
+                    <div className="py-16 flex flex-col justify-between" style={{ height: 'calc(100% - 64px)' }}>
                         <ul className="flex gap-6">
                             <li><a className="uppercase p-4 font-semibold" href="#">Home</a></li>
                             {/* <li className="uppercase p-4 font-semibold"><a href="#">Pricing</a></li>
@@ -48,7 +51,7 @@ export default function WebLayout({children}){
                             <li className="uppercase p-4 font-semibold"><a href="#">Company</a></li>
                             <a className="p-4 uppercase font-semibold" href="#">Login</a> */}
                         </ul>
-                        <button className="py-4 px-9 flex items-center justify-center gap-2 bg-pd-red text-pd-white rounded-[3.25rem] font-medium pd-button font-montserrat">Go To Cart<ArrowRight className="h-5 w-5"/></button>
+                        <button className="mx-4 py-4 px-9 flex items-center justify-center gap-2 bg-pd-red text-pd-white rounded-[3.25rem] font-medium pd-button font-montserrat">Go To Cart<ArrowRight className="h-5 w-5"/></button>
                     </div>
                 </nav>
             </div>
