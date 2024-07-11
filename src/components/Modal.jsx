14f2cf606d0f191e9c8bc7dd3afe71cc929c7ba3
Russell Oje <russelloje@gmail.com>
2024-07-11 08:18:47 +0100

    Changed animated success icon

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ import Close from "../assets/img/icons/Close.svg";
 export default function Modal({children, open, setOpen}){
     return (
         <div className={`${open?'flex':'hidden'} fixed top-0 left-0 w-screen h-screen z-[90] bg-pd-black/60 justify-center items-center`}>
-            <div className="max-h-[90vh] overflow-y-scroll shadow-lg border border-black border-solid bg-pd-white rounded-xl w-[380px] max-w-[90%] mx-auto">
+            <div className="max-h-[95vh] overflow-y-auto shadow-lg border border-black border-solid bg-pd-white rounded-xl w-[380px] max-w-[90%] mx-auto">
                 <div className="p-8 leading-[150%]">
                     <div className="flex justify-end">
                         <button onClick={()=>setOpen(false)}>
--- src/components/parts/ShowSuccess.jsx
+++ src/components/parts/ShowSuccess.jsx
@@ -1,4 +1,5 @@
-import tick from "../../assets/img/icons/tick.svg";
+import { Link } from 'react-router-dom';
+import tick from "../../assets/img/icons/tick_red.gif";
 
 export default function ShowSuccess() {
   return (
@@ -7,7 +8,7 @@ export default function ShowSuccess() {
         <h3 className="mt-12 md:mt-16 pd-h3 leading-8 text-center text-pd-black">Your Order is confirmed!</h3>
         <p className="pd-p mt-4 text-base leading-6 text-center text-black">We’ll send you a shipping confirmation email as soon as your order
         ships.</p>
-        <button className="mt-10 mb-5 py-4 px-10 flex items-center justify-center gap-2 bg-pd-red text-pd-white rounded-[3.25rem] font-medium pd-button font-montserrat">Continue Shopping</button>
+        <Link to="/" className="mt-10 mb-5 py-4 px-10 flex items-center justify-center gap-2 bg-pd-red text-pd-white rounded-[3.25rem] font-medium pd-button font-montserrat">Continue Shopping</Link>
     </div>
   );
 }
